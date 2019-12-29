b5fdd6f658d7b6f6f6c43cdb16d2efdb32cc29b2
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-12-29 12:28:25 +0000

    siri

---
+++ css/src/components/contacts.js
@@ -0,0 +1,59 @@
+import React, {Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent  } from 'react-mdl';
+
+class Contacts extends Component {
+    render() {
+        return(
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2> Valeriu Olariu </h2>
+                        <img
+                        src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-152.png"
+                        alt="avatart"
+                        style={{height: '250px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>adable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lor </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2> 
+                        <hr/>
+                       <div className="contact-list">
+                       <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-phone-square" aria-hidden="true"/> 
+                                    (778) 745-3132
+                                </ListItemContent>
+                            </ListItem>
+                            
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-fax" aria-hidden="true"/> 
+                                    (778) 745-3132
+                                </ListItemContent>
+                            </ListItem>
+                            
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-envelope" aria-hidden="true"/> 
+                                    someone@example.com
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className="fa fa-skype" aria-hidden="true"/> 
+                                    MySkypeId
+                                </ListItemContent>
+                            </ListItem>
+                        </List>
+                       </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Contacts;
